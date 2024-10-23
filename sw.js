6148e864b252d417c84e83d9c1e1fb7b80c59d5e
codeAlphaBravosite <help.factchannel@gmail.com>
2024-10-23 15:59:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = '1.0.5';
-const CACHE_NAME = `noteepadd-cache-${VERSION}`;
+const VERSION = '1.0.2';
+const CACHE_NAME = `voicenote-cache-${VERSION}`;
 
 const STATIC_CACHE_URLS = [
   './',
@@ -22,7 +22,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName.startsWith('noteepadd-cache-') && cacheName !== CACHE_NAME) {
+          if (cacheName.startsWith('voicenote-cache-') && cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
